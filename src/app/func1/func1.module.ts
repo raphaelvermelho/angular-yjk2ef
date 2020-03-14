9d51201e8eec04c33b17a160f6148f9332151696
raphaelvermelho <raphaelvermelho@gmail.com>
2020-03-14 16:47:10 -0300

    2 files modified

--- src/app/func1/func1.module.ts
+++ src/app/func1/func1.module.ts
@@ -1,12 +1,13 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import { Comp1Component } from './comp1/comp1.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
 
     declarations: [Comp1Component], // Componentes do Móódulo
     exports: [Comp1Component], // 
-    imports: [CommonModule]
+    imports: [CommonModule, FormsModule]
     
 })
 
