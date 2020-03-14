d8d87fa2b1e8c96874f4c53a448e6fea6eeb3d71
raphaelvermelho <raphaelvermelho@gmail.com>
2020-03-14 14:14:41 -0300

    app.component.html - Pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
 
-    imports : [], // Outros Módulos Usados
+    imports : [BrowserModule], // Outros Módulos Usados
     exports : [], // Vou exportar (tornar visível) um componente/pipe...
     providers : [], // É o conjunto de serviços que estarão disponíveis para este módulo...
     declarations : [], // É o conjunto de componentes do módulo...
