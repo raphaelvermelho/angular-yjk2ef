1c10285604b9a915b18512e21cfb8df9c41d7a1d
raphaelvermelho <raphaelvermelho@gmail.com>
2020-03-14 14:59:41 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 
 import {AppComponent} from './app.component';
+import {componente1Componente} from './componente1/appcomponente1';
 
 @NgModule({
 
     imports : [BrowserModule], // Outros Módulos Usados
     exports : [], // Vou exportar (tornar visível) um componente/pipe...
     providers : [], // É o conjunto de serviços que estarão disponíveis para este módulo...
-    declarations : [AppComponent], // É o conjunto de componentes do módulo...
+    declarations : [AppComponent, appcomponente1], // É o conjunto de componentes do módulo...
     bootstrap : [AppComponent], // É o conjunto de componentes pelo qual a aplicação web vai iniciar a execução
     
 })
