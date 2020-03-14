b80aa04368c2c77bb9bc0ff4d23ce851166b0b92
raphaelvermelho <raphaelvermelho@gmail.com>
2020-03-14 14:09:20 -0300

    App.Component.ts - Pronto

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+// Decorator para transformar esta classe em um componente...
+
+  selector: 'app-main', // Seletor HTML
+  templateUrl: './app.component.html', // HTML do componente
+  styleUrls: [] // Folhas de Estilo(CSS) que estabilizam o componente.
+
+})
+
+export class AppComponent{
+
+
+}
