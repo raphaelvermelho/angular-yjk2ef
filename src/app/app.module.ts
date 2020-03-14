d1ef70909234ca06ab4b58420b5105b23e9076f9
raphaelvermelho <raphaelvermelho@gmail.com>
2020-03-14 15:06:28 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 
 import {AppComponent} from './app.component';
-import {componente1Componente} from './componente1/app-componente1';
+import {componente1Componente} from './componente1/componente1.componente';
 
 @NgModule({
 
