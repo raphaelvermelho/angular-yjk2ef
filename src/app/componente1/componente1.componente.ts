2ea526c615ffaa58906480f70066145a1c1ffd2b
raphaelvermelho <raphaelvermelho@gmail.com>
2020-03-14 14:51:44 -0300

    Componente1

---
+++ src/app/componente1/componente1.componente.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+
+@Component({
+  
+  selector: 'app-componente1', // Seletor HTML
+  templateUrl: './app.componente1.html', // HTML do componente
+  styleUrls: [] // Folhas de Estilo(CSS) que estabilizam o componente.
+
+})
