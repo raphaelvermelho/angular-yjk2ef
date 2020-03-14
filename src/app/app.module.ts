99c1c4ea6c1aabddf50e806d947b30de3f227850
raphaelvermelho <raphaelvermelho@gmail.com>
2020-03-14 14:22:55 -0300

    Pronto - Primeira saída Site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 
+import {AppComponent} from './app.component';
+
 @NgModule({
 
     imports : [BrowserModule], // Outros Módulos Usados
     exports : [], // Vou exportar (tornar visível) um componente/pipe...
     providers : [], // É o conjunto de serviços que estarão disponíveis para este módulo...
-    declarations : [], // É o conjunto de componentes do módulo...
-    bootstrap : [], // É o conjunto de componentes pelo qual a aplicação web vai iniciar a execução
+    declarations : [AppComponent], // É o conjunto de componentes do módulo...
+    bootstrap : [AppComponent], // É o conjunto de componentes pelo qual a aplicação web vai iniciar a execução
     
 })
 
